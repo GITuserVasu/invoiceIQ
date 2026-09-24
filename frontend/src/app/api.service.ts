3c435d5ac33513054c8492c1cb4ffafa71e31c5b
GITuserVasu <110546548+GITuserVasu@users.noreply.github.com>
2026-09-23 20:20:16 -0700

    still
    trying

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -7,7 +7,7 @@ export class ApiService {
   private readonly http = inject(HttpClient);
   //private readonly baseUrl = environment.apiUrl;
   tenantName = environment.TENANTNAME;
-  baseUrl = "http://127.0.0.1:7070";
+  baseUrl = "http://127.0.0.1:8080";
 
   getTenant(tenantKey?: string) {
     const storedTenantId = localStorage.getItem('lx_tenant_id');
